219ab0fee7e692fc8a714375ecdef3d7c18473d2
JuanM0412 <juangomezp1204@gmail.com>
2025-02-24 15:59:44 -0500

    refactor: alert creation to use service directly and implement main FastAPI application

--- backend/src/modules/alerts/routes.py
+++ backend/src/modules/alerts/routes.py
@@ -1,10 +1,22 @@
 from fastapi import APIRouter, Depends
-from .controllers import AlertController
 from .models import AlertInput, AlertDB
-from typing import List
+from .service import AlertService
 
 router = APIRouter()
 
 @router.post("/create", response_model=AlertDB)
-async def create_alert(alert_data: AlertInput, controller: AlertController = Depends()):
-    return await controller.create_alert(alert_data)
+async def create_alert(alert_data: AlertInput, alert_service: AlertService = Depends()):
+    """
+    Creates a new alert in the database.
+
+    Args:
+
+        alert_data (AlertInput): Data required to create the alert.
+
+        alert_service (AlertService, optional): Service instance to handle alert creation. Defaults to Depends().
+
+    Returns:
+    
+        AlertDB: The created alert with its assigned ID.
+    """
+    return await alert_service.create_alert(alert_data)
