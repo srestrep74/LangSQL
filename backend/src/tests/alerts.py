3f0363b9d9386b1a3090e0ce983fcddc0d1583a2
JuanM0412 <juangomezp1204@gmail.com>
2025-04-15 19:58:42 -0500

    test: refactor alert tests to include alert creation and deletion logic

--- backend/src/tests/alerts.py
+++ backend/src/tests/alerts.py
@@ -1,10 +1,11 @@
 from unittest.mock import MagicMock, patch
 
-import pytest
+import pytest, json
+from datetime import datetime
 from fastapi.testclient import TestClient
 
 from app import app
-from src.modules.text_to_sql.service import SyntheticDataModelService
+from src.modules.alerts.models.models import AlertCreate
 from src.modules.text_to_sql.utils.APIClientLLMClient import APIClientLLMClient
 from src.tests.utils.mock_db_structure import MOCK_DB_STRUCTURE
 from src.modules.queries.schemas.DatabaseConnection import DatabaseConnection
@@ -21,40 +22,35 @@ database_connection = DatabaseConnection(
     schema_name="inventory"
 )
 
+class TestAlert:
+    def test_create_alert(self):
+        alert_data = AlertCreate(
+            notification_emails=["test@test.com"],
+            user="test_user",
+            prompt="Most expensive product",
+            sent=False,
+            expiration_date=datetime.utcnow(),
+        )
 
-class TestSyntheticData:
-    @patch("src.modules.text_to_sql.service.SyntheticDataModelService.generate_synthetic_data")
-    def test_generate_synthetic_data_endpoint(self, mock_generate_synthetic_data):
-        mock_generate_synthetic_data.return_value = [
-            "INSERT INTO inventory.category (id, name) VALUES (361, 'Hardware');"
-        ]
+        alert_dict = alert_data.model_dump()
+        alert_dict["expiration_date"] = alert_dict["expiration_date"].isoformat()
+
+        connection_dict = database_connection.model_dump()
 
         response = client.post(
-            "/api/text-to-sql/generate_synthetic_data",
-            json={"iterations": 1, "connection": database_connection.model_dump()}
+            "/api/alerts/create",
+            data=json.dumps({
+                "connection": connection_dict,
+                "alert_data": alert_dict
+            }, default=str),
+            headers={"Content-Type": "application/json"}
         )
 
         assert response.status_code == 200
+        assert response.json()["message"] == "Success"
+        assert response.json()["data"]["user"] == alert_dict["user"]
 
-        response_json = response.json()
-        assert response_json["status"] == "success"
-        assert response_json["message"] == "Success"
-        assert "results" in response_json["data"]
-
-        mock_generate_synthetic_data.assert_called_once_with(1, database_connection)
-
-    @pytest.fixture
-    def mock_synthetic_data_service(self):
-        mock_query_adapter = MagicMock()
-        mock_query_adapter.get_db_structure.return_value = MOCK_DB_STRUCTURE
-        mock_query_adapter.execute_query.return_value = None
-
-        llm_client = APIClientLLMClient()
-
-        return SyntheticDataModelService(mock_query_adapter, llm_client)
-
-    def test_generate_synthetic_data_service(self, mock_synthetic_data_service):
-        response = mock_synthetic_data_service.generate_synthetic_data(iterations=40, connection=database_connection)
-
-        assert isinstance(response, str)
-        assert "INSERT INTO" in response
+        alert_id = response.json()["data"]["id"]
+        response = client.delete(
+            f"/api/alerts/{alert_id}?user_id={alert_dict['user']}"
+        )
